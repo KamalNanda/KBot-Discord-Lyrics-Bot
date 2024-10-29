65d27ba1d3f5e51f1cec4d31228c4d648f25bd11
Kamal Nanda <kamalnanda45@gmail.com>
2024-10-29 13:01:59 +0530

    Update scrape_lyrics.js

--- scrape_lyrics.js
+++ scrape_lyrics.js
@@ -1,6 +1,6 @@
 const cheerio = require('cheerio');
 const request = require('request-promise');
-const scrape_lyrics = async (url , message) => {
+export const scrape_lyrics = async (url , message) => {
 	const response = await request(url)
 	let $ = cheerio.load(response)
 	let lyrics = $('body > div.container.main-page > div > div.col-xs-12.col-lg-8.text-center > div:nth-child(10)').text()
